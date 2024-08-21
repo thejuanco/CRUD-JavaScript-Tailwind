548b1c18ec95b43a6fae64eb405aa93285b5caab
Juanco <juanffo874@gmail.com>
2024-08-20 19:37:30 -0600

    Crea las funcion para cargar el documento y añade los selectores del documento

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+    //Selectores 
+    const form = document.querySelector('formulario')
+    const name = document.querySelector('#username')
+    const email = document.querySelector('#useremail')
+
+    //Eventlisteners
+    listeners()
+    function listeners(){
+        // form.addEventListener('submit', addUser)
+        name.addEventListener('change', validate)
+        email.addEventListener('change', validate)
+    }
+
+    function validate(){
+        console.log('Validando')
+    }
 })
