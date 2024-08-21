57af6630b41bd845ecb5c909d7cd51130a92396e
Juanco <juanffo874@gmail.com>
2024-08-20 20:45:01 -0600

    Evita duplicar la alerta si ya existe, y quita la alerta despues de 3 segundos

--- js/alerts.js
+++ js/alerts.js
@@ -1,13 +1,20 @@
-const form = document.querySelector("#alertDiv");
-
 export const alert = message => {
-    const divMessage = document.createElement('div')
-    divMessage.classList.add('p-2', 'rounded-md', 'border', 'border-red-400', 'w-80')
-    
-    const messageText = document.createElement('p')
-    messageText.classList.add('font-semibold', 'text-red-500', 'text-center')
-    messageText.textContent = message
+    const form = document.querySelector("#alertDiv");
+    const alertExists = document.querySelector(".alert")
 
-    divMessage.appendChild(messageText)
-    form.appendChild(divMessage)
+    if(!alertExists){
+        const divMessage = document.createElement('div')
+        divMessage.classList.add('p-2', 'rounded-md', 'border', 'border-red-400', 'w-80', 'alert')
+        
+        const messageText = document.createElement('p')
+        messageText.classList.add('font-semibold', 'text-red-500', 'text-center')
+        messageText.textContent = message
+
+        divMessage.appendChild(messageText)
+        form.appendChild(divMessage)
+
+        setTimeout(() => {
+            divMessage.remove()
+        }, 3000)
+    }
 }
